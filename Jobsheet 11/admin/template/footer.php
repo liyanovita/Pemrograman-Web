fdbf0425a84a81870febe440a36b2a0220dfbdf9
liyanovita <143494396+liyanovita@users.noreply.github.com>
2024-04-15 20:12:08 +0700

    Menambahkan

---
+++ Jobsheet 11/admin/template/footer.php
@@ -0,0 +1,13 @@
+<!-- Praktikum 3. Folder Template
+Buat satu file baru bernama footer.php di dalam folder template. Ketikkan kode di bawah ini
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-e17PSr3L1XLISH8JdDII5YN/
+njoSsxfbrkCTnJrzXt+ENP5M0VBxD+16sEG4zoLp" crossorigin="anonymous"></script>
+<script src="assets/custom/dashboard.js"></script>
+</body>
+</html>
